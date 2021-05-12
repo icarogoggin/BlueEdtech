7b6937130f348df124df57040303958b4bd154f1
icarogoggin <icaro.goggin@gmail.com>
2021-05-12 18:55:41 -0300

    Delete exercicios01.py

--- Aula06_Exercicios/exercicios01.py
+++
@@ -1,8 +0,0 @@
-def soma(x1, y1, z1):
-    soma = x1 + y1 + z1
-    print (f'O resultado da sua soma é : {soma}')
-### Programa 01 - Soma ###
-x1 = int(input(' Escreva o primeiro número: '))
-y1 = int(input(' Escreva o segundo número: '))
-z1 = int(input(' Escreva o terceiro número: '))
-soma(x1, y1, z1)
