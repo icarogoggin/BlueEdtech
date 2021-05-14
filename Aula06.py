529d845e2f2ae00dead763e710f770e2206d1df8
icarogoggin <icaro.goggin@gmail.com>
2021-05-14 14:46:18 -0300

    Create Aula06.py

---
+++ Aula06.py
@@ -0,0 +1,18 @@
+lista1 = [5, 6, 7]
+lista2 = ["A", "B", "C"]
+lista3 = [1, 2, "um", "dois"]
+lista4 = [10, 20, [30, 40]]
+lista4 = [10, 20, [30, 40, [50, 60]]]
+
+# print(lista1[0])
+# print(lista2[2])
+# print(lista3)
+# print(lista3[0])
+# print(lista3[3])
+
+print(lista4)
+print(lista4[0])
+print(lista4[1])
+print(lista4[2])
+print(lista4[2][0])
+print(lista4[2][2])
