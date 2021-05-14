8d0744a68a57b3566af24d67578a5eee2ab03a34
icarogoggin <icaro.goggin@gmail.com>
2021-05-14 19:00:35 -0300

    Update exercicios03.py

--- Modulo 1/Aula07_Exercicios/exercicios03.py
+++ Modulo 1/Aula07_Exercicios/exercicios03.py
@@ -1,3 +1,7 @@
 '''Crie uma função que receba uma string como argumento e retorne a mesma string em letras maiúsculas. 
 Faça uma chamada à função, passando como parâmetro uma string.'''
+def upper():
+    upper = input('Escreva aqui sua mensagem: ').upper()
+    print(upper)
 
+upper()
