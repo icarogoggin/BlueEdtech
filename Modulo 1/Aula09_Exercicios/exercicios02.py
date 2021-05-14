ede33c70007afe0f5fb2015bae2ddf70190afb5e
icarogoggin <icaro.goggin@gmail.com>
2021-05-14 14:47:03 -0300

    Create exercicios02.py

---
+++ Modulo 1/Aula09_Exercicios/exercicios02.py
@@ -0,0 +1,3 @@
+#2.	Crie um código em Python para exibir a contagem de dígitos de um número inteiro positivo informado pelo usuário.
+frase = (input('Digite o número inteiro positivo: '))
+print('O número tem', len(frase), 'dígitos.')
