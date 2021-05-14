1c4703ddf83d9a62b9e85b1a1b442d3925a46dd3
icarogoggin <icaro.goggin@gmail.com>
2021-05-14 14:46:30 -0300

    Create exercicios05.py

---
+++ Modulo 1/Aula07_Exercicios/exercicios05.py
@@ -0,0 +1,3 @@
+'''Exercício 5 - Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros: o nome de um jogador e quantos gols ele marcou.
+O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+'''
