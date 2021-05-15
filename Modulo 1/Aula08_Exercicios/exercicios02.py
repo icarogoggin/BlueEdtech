a1acf9f6e2e1279308599edf2d1b07a041e944f8
icarogoggin <icaro.goggin@gmail.com>
2021-05-15 17:40:11 -0300

    Update exercicios02.py

--- Modulo 1/Aula08_Exercicios/exercicios02.py
+++ Modulo 1/Aula08_Exercicios/exercicios02.py
@@ -2,7 +2,7 @@
 O programa terá uma lista de palavras lidas de um arquivo texto e escolherá uma aleatoriamente.
  O jogador poderá errar 6 vezes antes de ser enforcado.'''
 
-palavra = 'python'.upper()
+palavra = 'programar'.upper()
 acertos = 0
 erros = 0
 letras_acertadas = ''
