7bf9501f8c147c5d4e473356fe444111ea6f6164
icarogoggin <icaro.goggin@gmail.com>
2021-05-15 17:40:25 -0300

    Update exercicios06.py

--- Modulo 1/Aula08_Exercicios/exercicios06.py
+++ Modulo 1/Aula08_Exercicios/exercicios06.py
@@ -1,1 +1,9 @@
 '''6.	Escreva um programa onde o usuário digita uma frase e essa frase retorna sem nenhuma vogal.'''
+
+frase = input('Digite aqui sua frase: ')
+exclusao = 'aeiou'
+
+for i in exclusao:
+    frase = frase.replace(i, '')
+    
+print(frase)
