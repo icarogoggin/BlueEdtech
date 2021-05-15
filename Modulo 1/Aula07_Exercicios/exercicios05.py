71230c091386750c6fa900ff9fd0ac9ea9c7f35b
icarogoggin <icaro.goggin@gmail.com>
2021-05-15 17:39:29 -0300

    Update exercicios05.py

--- Modulo 1/Aula07_Exercicios/exercicios05.py
+++ Modulo 1/Aula07_Exercicios/exercicios05.py
@@ -1,3 +1,16 @@
 '''Exercício 5 - Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros: o nome de um jogador e quantos gols ele marcou.
 O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 '''
+#nome do jogador
+#qts gols
+jogador = []
+gols = []
+
+def ficha ():
+    nome_jogador = input('Digite o nome do jogador: ')
+    jogador.append(nome_jogador)
+    qtd_gols = input('Digite a quantidade de gols marcados: ')
+    gols.append(qtd_gols)
+
+ficha()
+print(f'O jogador',jogador[0], 'fez', gols[0], 'gols até o momento em sua carreira!')
