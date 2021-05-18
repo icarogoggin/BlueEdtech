95c4a2eb891d67fafaf1ccbb837f992f7cbd3fde
icarogoggin <icaro.goggin@gmail.com>
2021-05-17 22:17:08 -0300

    Create Fixacao_01.py

---
+++ Modulo 1/Aula11_exercicios/Fixacao_01.py
@@ -0,0 +1,11 @@
+#criando a lista p o dicionário
+lista01 = [('zé', '123-123'), ('joão','234-234'), ('Maria','345-345'), ('Joana','456-456'), ('Pedro','567-567')]
+#print(lista01[0])
+#print(lista01[0][0])
+contatos = dict(lista01)
+print(type(contatos))
+print(contatos)
+
+'''#dicionário
+contatos = {'Pedro':'123-123'}# dicionário é composto por {} e : é como se fosse o = para uma variável
+print(contatos)'''
