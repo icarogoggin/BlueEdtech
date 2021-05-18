05e093e731742f46e2a3841aa5e4e48e3ce22665
icarogoggin <icaro.goggin@gmail.com>
2021-05-17 23:02:26 -0300

    Update Fixacao_01.py

--- Modulo 1/Aula11_exercicios/Fixacao_01.py
+++ Modulo 1/Aula11_exercicios/Fixacao_01.py
@@ -1,11 +1,21 @@
-#criando a lista p o dicionário
+'''#criando a lista p o dicionário
 lista01 = [('zé', '123-123'), ('joão','234-234'), ('Maria','345-345'), ('Joana','456-456'), ('Pedro','567-567')]
 #print(lista01[0])
 #print(lista01[0][0])
 contatos = dict(lista01)
 print(type(contatos))
 print(contatos)
+nome = input('Digite um nome para pesquisar')
+print(contatos.get(nome,'nome não encontrado'))
 
-'''#dicionário
+
+#dicionário
 contatos = {'Pedro':'123-123'}# dicionário é composto por {} e : é como se fosse o = para uma variável
 print(contatos)'''
+
+
+elenco = [('Chris Evans', 'Capitão America'), ('Mark Ruffalo', 'Hulk'), ('Tom Hiddleston', 'Loki'), ('Chris Hemsworth','Thor'), ('Robert Downey Jr', 'Homem de Ferro'), ('Scarlett Johansson','Viúva Negra')]
+vingadores = dict(elenco)
+nome_ator = input('Digite o nome de um dos atores: ')
+print(vingadores.get(nome_ator,'Esse nome não está no elenco!'))
+print('O elenco dos vingadores é composto por: ',vingadores)
