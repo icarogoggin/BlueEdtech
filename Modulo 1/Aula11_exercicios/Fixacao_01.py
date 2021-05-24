60b7af18068a1e391acf92d958bed519e76036a6
icarogoggin <icaro.goggin@gmail.com>
2021-05-23 21:31:40 -0300

    Update Fixacao_01.py

--- Modulo 1/Aula11_exercicios/Fixacao_01.py
+++ Modulo 1/Aula11_exercicios/Fixacao_01.py
@@ -1,21 +1,41 @@
-'''#criando a lista p o dicionário
-lista01 = [('zé', '123-123'), ('joão','234-234'), ('Maria','345-345'), ('Joana','456-456'), ('Pedro','567-567')]
-#print(lista01[0])
-#print(lista01[0][0])
-contatos = dict(lista01)
-print(type(contatos))
+"""
+lista1 = [1, 2, 3, "String", "String2"]
+tupla1 = ("Nome","123-456")
+lista3 = [("Nome","123-456"), 2, 3, "String", "String2"] # Coloquei a tupla dentro da lista, como um dos elementos dela
+"""
+
+# Criando lista com tuplas
+lista_contatos = [("Ana Paula","123-456"),("Joao","456-789"),("Fabricio","444-777"),("Ricardo","777-888"),("Bruno","999-888")]
+print(len(lista_contatos))
+print(type(lista_contatos))
+print(lista_contatos)
+print(lista_contatos[0])
+print(lista_contatos[0][0])
+print()
+
+# Criando um dicionário a partir da lista acima com a função dict()
+contatos = dict(lista_contatos) # dicionario = dict(lista_a_ser_convertida)
 print(contatos)
-nome = input('Digite um nome para pesquisar')
-print(contatos.get(nome,'nome não encontrado'))
+print("Type de contatos:")
+print(type(contatos))
+print()
+
+# Acessando um valor dentro de um dicionário
+print('Acessando o valor da chave "Ana Paula"')
+print(contatos.get("Ana Paula"))
+print(contatos["Ana Paula"])
+
+# print(contatos["Eurico"]) # KeyError - A chave não existe
+
+print(contatos.get("Eurico","Nome não encontrado")) # Retorna um valor padrão caso a chave não seja encontrada
+
+nome = input("Digite o nome: ") # Recebendo uma entrada para procurar o valor
+print(contatos.get(nome,"Nome não encontrado")) 
 
 
-#dicionário
-contatos = {'Pedro':'123-123'}# dicionário é composto por {} e : é como se fosse o = para uma variável
-print(contatos)'''
-
-
-elenco = [('Chris Evans', 'Capitão America'), ('Mark Ruffalo', 'Hulk'), ('Tom Hiddleston', 'Loki'), ('Chris Hemsworth','Thor'), ('Robert Downey Jr', 'Homem de Ferro'), ('Scarlett Johansson','Viúva Negra')]
-vingadores = dict(elenco)
-nome_ator = input('Digite o nome de um dos atores: ')
-print(vingadores.get(nome_ator,'Esse nome não está no elenco!'))
-print('O elenco dos vingadores é composto por: ',vingadores)
+#Criando um dicionario "na mão"
+"""
+dicionario_contatos = {"Alexandre":"555-444", "Talita":"111-222", "Nadja":"666-333"}
+print(dicionario_contatos)
+print(len(dicionario_contatos))
+"""
